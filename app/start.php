8b8b18c07e617d217cae815d427fa082eba04fce
mr.Diko <mr.diko89@gmail.com>
2015-07-27 09:34:18 +0300

    Add admin panel

--- app/start.php
+++ app/start.php
@@ -3,11 +3,11 @@
 ini_set('display_errors', 'on');
 
 define('APP_ROOT', __DIR__);
-define('VIEW_ROOT', APP_ROOT . '/views');
-define('BASE_URL', 'http://localhost/cms');
+define('VIEW_ROOT', APP_ROOT . '\views');
+define('BASE_URL', 'http://cms');
 
 try{
-	$db = new PDO('mysql:host=127.0.0.1;dbname=cms', 'root', 'root');
+	$db = new PDO('mysql:host=127.0.0.1;dbname=cms', 'root', '');
 }catch(PDOException $ex){
 	echo "Ther is an error".$ex->getMessage();
 }
