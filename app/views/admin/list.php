1f5b9ff2c515c9e652f626232a5689f82446553d
mr.Diko <mr.diko89@gmail.com>
2015-07-28 01:15:00 +0300

    add editing

--- app/views/admin/list.php
+++ app/views/admin/list.php
@@ -19,7 +19,7 @@
 					<td><?php echo e($page['label']); ?></td>
 					<td><?php echo e($page['title']); ?></td>
 					<td><a href="<?php echo BASE_URL; ?>/page.php?page=<?php echo e($page['slug']);?>"><?php echo e($page['label']); ?></a></td>
-					<td><a href="">Edit</a></td>
+					<td><a href="<?php  echo BASE_URL; ?>/admin/edit.php?id=<?php echo e($page['id']); ?>">Edit</a></td>
 					<td><a href="">Delete</a></td>
 				</tr>
 			<?php endforeach; ?>
